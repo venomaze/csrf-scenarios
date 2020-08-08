273596b0fada5b40bb5b9295a9514e9c20f4b2fc
venomaze <venomaze@protonmail.com>
2020-08-08 11:28:04 -0700

    Add attacker to scenario 3

---
+++ scenario-3/attacker/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const ip = require('ip');
+
+const app = express();
+const PORT = 3001;
+const IP = ip.address();
+const target = `http://${IP}:3000`;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/delete', (req, res) => {
+  res.render('delete', {
+    target,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Attacker running on port ${PORT}`);
+});
