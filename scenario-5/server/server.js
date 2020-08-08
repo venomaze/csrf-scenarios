cbcd92cabf76cc0e2b1685448843d48faa494fa6
venomaze <venomaze@protonmail.com>
2020-08-08 11:46:46 -0700

    Add server to scenario 5

---
+++ scenario-5/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const ip = require('ip');
+
+const isAdmin = require('./middlewares/admin');
+const adminRouter = require('./routes/admin');
+const indexRouter = require('./routes');
+
+const app = express();
+const IP = ip.address();
+const PORT = 3000;
+
+const admin = { active: true };
+
+app.use(
+  session({
+    secret: 'secret_key',
+    cookie: {
+      sameSite: 'none',
+      httpOnly: true,
+    },
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('admin', admin);
+
+app.use('/', indexRouter);
+app.use('/admin', isAdmin, adminRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://${IP}:${PORT}`);
+});
